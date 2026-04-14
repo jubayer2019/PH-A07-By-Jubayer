e2cd178616606c5908b85c358ce4ec34c41a37eb
jubayer2019 <jubayer.prodesigner@gmail.com>
2026-04-14 11:28:52 -0400

    Not Found Page Added

---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const notfound = () => {
+  return (
+    <div className='flex h-150 w-full justify-center items-center'><h2 className='text-6xl font-extrabold text-center text-[#244D3F]'>404 | Not Found</h2></div>
+  )
+}
+
+export default notfound
